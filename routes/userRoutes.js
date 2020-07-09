7a38791ac49e0d90d5aff0da92fd654827a443f7
akgupta-47 <60443592+akgupta-47@users.noreply.github.com>
2020-07-09 23:40:46 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,3 +1,10 @@
-const express = require("express");
-
+const express = require('express');
+const postController = require('../controllers/postControler');
 const router = express.Router();
+
+router
+  .route('/')
+  .get(postController.getAllPosts)
+  .post(postController.createPost);
+
+module.exports = router;
