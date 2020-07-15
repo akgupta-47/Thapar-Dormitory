05e4eeafd3f645d76aca20d06462f7d11371faec
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2020-07-16 00:56:41 +0530

    Changes to create laundryReciept route and changed reciept delete functionality

--- routes/laundryRoutes.js
+++ routes/laundryRoutes.js
@@ -1,13 +1,27 @@
-const express = require('express');
-const laundryController = require('../controllers/laundryController');
+const express = require("express");
+const laundryController = require("../controllers/laundryController");
 const router = express.Router();
-const auth = require('../middleware/auth');
-
-router.route('/').post(auth, laundryController.createReceipt);
+const auth = require("../middleware/auth");
+const { check } = require("express-validator");
 
 router
-  .route('/:id')
-  .get(auth, laundryController.getReceipt)
+  .route("/")
+  .post(
+    [
+      auth,
+      check(
+        "laundryNumber",
+        "You must provide the laundry number of the student to create a new entry!"
+      )
+        .not()
+        .isEmpty(),
+    ],
+    laundryController.createReceipt
+  );
+
+router
+  .route("/:id")
+  .put(auth, laundryController.updateReceipt)
   .delete(auth, laundryController.deleteReceipt);
 
 module.exports = router;
